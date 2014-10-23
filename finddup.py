022a2c461f6e8e2f5aceb680fa3c5499f4c92dfe
Jeff Leverenz <jeff.leverenz@gmail.com>
2014-10-23 05:12:58 -0500

    Added logging to show progress.

--- finddup.py
+++ finddup.py
@@ -7,7 +7,9 @@ import os
 import subprocess
 import sys
 
-# Simple logging class to support --verbose switch
+from math import factorial
+
+# Simple logging class to support verbosity log levels
 class Log():
     is_verbose = False
     of = sys.stdout
@@ -46,10 +48,20 @@ class FileComparer:
 
         # For each pair, we are checking if the second is a duplicate of the
         # first. This maintains the original preference order.
+        i = 0
+        total = nCr(len(self.filelist), 2)
         for pair in pairs:
+            i += 1
             # Skip if the second has already been determined to be a duplicate
             if pair[1] in checked:
+                Log.verbose("Comparing ({}/{}): already identified as duplicate, skipping:".format(i,total))
+                Log.verbose("   " + pair[1])
+                Log.verbose("   " + pair[0])
                 continue
+
+            Log.verbose("Comparing ({}/{}):".format(i,total))
+            Log.verbose("   " + pair[1])
+            Log.verbose("   " + pair[0])
             if self.__comparitor(pair[1], pair[0]):
                 duplicate_files.append((pair[1], pair[0])) # duplicate, and original
                 checked.append(pair[1])                    # track duplicates found
@@ -86,25 +98,32 @@ def groupBySize(filelist):
 
 # duplicate_original is a tuple with the duplicate listed first
 def outputDuplicateFile(duplicates):
-    if Log.is_verbose:
-        Log.verbose(duplicates[0] + " (dupe of " + duplicates[1] + ")")
-    else:
-        Log.log(duplicates[0])
+    Log.log(duplicates[0])
 
+
+# combinations for small n,r
+def nCr(n,r):
+    return factorial(n) / (factorial(r) * factorial(n-r))
 
 # Return a list of files from filelist that are duplicates. For any group of
 # duplicate files, all but one will be included in the list
 def compareFiles(filelist):
+    Log.verbose(str(len(filelist)) + " files to be examined")
+
     # Hash by file size and create files_to_compare, a list of lists of files
     # with the same size.
     sizegroup = groupBySize(filelist)
     files_to_compare = [i[1] for i in sizegroup.iteritems() if len(i[1]) > 1]
 
+    combinations = reduce(lambda a,i: a + nCr(len(i), 2), files_to_compare, 0)
+
     duplicate_files = []
-    for i in files_to_compare:
-        f = FileComparer(*i)
+    for comp_files in files_to_compare:
+        Log.verbose("{} diffs left to complete".format(combinations))
+        f = FileComparer(*comp_files)
         dupes = f.compare()
         duplicate_files += dupes
         for i in dupes:
             outputDuplicateFile(i)
+        combinations -= nCr(len(comp_files), 2)
     return duplicate_files
