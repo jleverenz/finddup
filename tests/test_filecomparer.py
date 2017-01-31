7eac916da67c29d7aee26391daa37bcaef33b154
Jeff Leverenz <jeff.leverenz@gmail.com>
2017-01-31 01:27:48 -0600

    Convert test_filecomparer to use pyfakefs.

--- tests/test_filecomparer.py
+++ tests/test_filecomparer.py
@@ -1,13 +1,15 @@
 from test_helper import *
+from pyfakefs import fake_filesystem_unittest
 
 
-class TestFileComparer(unittest.TestCase):
+class TestFileComparer(fake_filesystem_unittest.TestCase):
 
     @staticmethod
     def compare(file1, file2):
         return file1.split('/')[-1] == file2.split('/')[-1]
 
     def setUp(self):
+        self.setUpPyfakefs()
         self.f = FileComparer()
         self.f.compareWith(self.compare)
 
@@ -15,29 +17,49 @@ class TestFileComparer(unittest.TestCase):
         f = FileComparer()
         self.assertEqual(f.filelist, [])
 
-        dirs = ['dir/file1', 'dir/file2']
-        f = FileComparer(*dirs)
-        self.assertEqual(f.filelist, dirs)
+        files = ['dir/file1', 'dir/file2']
+        f = FileComparer(*files)
+        self.assertEqual(f.filelist, files)
 
     def testAddFiles(self):
         f = FileComparer()
-        dirs = ['dir/file1', 'dir/file2']
-        f.addFiles(*dirs)
-        self.assertEqual(f.filelist, dirs)
+        files = ['dir/file1', 'dir/file2']
+        f.addFiles(*files)
+        self.assertEqual(f.filelist, files)
 
     def testCompareNoDuplicates(self):
-        self.f.addFiles('dir/file1', 'dir/file2', 'copy/file3')
+        self.fs.CreateFile('/test/file1.txt', contents='file1')
+        self.fs.CreateFile('/test/file2.txt', contents='file2')
+        self.fs.CreateFile('/test/file3.txt', contents='file3')
+
+        self.f.addFiles('/test/file1.txt',
+                        '/test/file2.txt',
+                        '/test/file3.txt')
         dups = self.f.compare()
         self.assertEqual(dups, [])
 
     def testCompareOneDuplicate(self):
-        self.f.addFiles('dir/file1', 'dir/file2', 'copy/file1')
+        self.fs.CreateFile('/test/file1.txt', contents='file1')
+        self.fs.CreateFile('/test/file2.txt', contents='file2')
+        self.fs.CreateFile('/copy/file1.txt', contents='file1')
+
+        self.f.addFiles('/test/file1.txt',
+                        '/test/file2.txt',
+                        '/copy/file1.txt')
         dups = self.f.compare()
-        self.assertEqual(dups, [('copy/file1', 'dir/file1')])
+        self.assertEqual(dups, [('/copy/file1.txt', '/test/file1.txt')])
 
     def testCompareWithMultipleMatches(self):
-        self.f.addFiles('dir/file1', 'dir/file2', 'copy/file1', 'other/file1')
+        self.fs.CreateFile('/test/file1.txt', contents='file1')
+        self.fs.CreateFile('/test/file2.txt', contents='file2')
+        self.fs.CreateFile('/copy/file1.txt', contents='file1')
+        self.fs.CreateFile('/other/file1.txt', contents='file1')
+
+        self.f.addFiles('/test/file1.txt',
+                        '/test/file2.txt',
+                        '/copy/file1.txt',
+                        '/other/file1.txt')
         dups = self.f.compare()
 
-        self.assertEqual(dups, [('copy/file1', 'dir/file1'),
-                                ('other/file1', 'dir/file1')])
+        self.assertEqual(dups, [('/copy/file1.txt', '/test/file1.txt'),
+                                ('/other/file1.txt', '/test/file1.txt')])
