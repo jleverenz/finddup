8735ccfa8d2dcf2f6b4b80ec1acdd43441ef4251
Jeff Leverenz <jeff.leverenz@gmail.com>
2017-02-05 08:31:26 -0600

    Set pty=True for invoke tasks to avoid buffered output.

--- tasks.py
+++ tasks.py
@@ -4,12 +4,12 @@ from invoke import task, run
 
 @task
 def test(ctx):
-    ctx.run("python -m unittest discover tests/")
-    ctx.run("pep8 finddup/ tests/")
+    ctx.run("python -m unittest discover tests/", pty=True)
+    ctx.run("pep8 finddup/ tests/", pty=True)
 
 @task
 def html(ctx, live=False):
     if live:
         ctx.run("sphinx-autobuild . _build/html -r \".git/.*\"", pty=True)
     else:
-        ctx.run("sphinx-build -M html . _build")
+        ctx.run("sphinx-build -M html . _build", pty=True)
