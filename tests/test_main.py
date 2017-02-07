e7e9247771ddfe2faf836ad7d6d24f380ed27032
Jeff Leverenz <jeff.leverenz@gmail.com>
2017-02-07 00:48:01 -0600

    Clean up main unit test, add test for verbose switch.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,10 @@
-from test_helper import *
-from pyfakefs import fake_filesystem_unittest
-from finddup.__main__ import main
+import io
 import sys
+
+from pyfakefs import fake_filesystem_unittest
+
+from test_helper import *
+from finddup.__main__ import main
 import finddup
 
 
@@ -10,7 +13,7 @@ class TestMain(fake_filesystem_unittest.TestCase):
     def setUp(self):
         self.setUpPyfakefs()
 
-    def test_it(self):
+    def test_output_switch(self):
         self.fs.CreateFile('/test/file1', contents='abc')
         self.fs.CreateFile('/test/file2', contents='abc')
         sys.argv = ['finddup', '/test', '--output', 'results.txt']
@@ -20,3 +23,21 @@ class TestMain(fake_filesystem_unittest.TestCase):
         # Test that the results file was created and is empty.
         with open('results.txt') as f:
             self.assertEqual(f.read().strip(), "/test/file2")
+
+    def test_verbose_switch(self):
+        self.fs.CreateFile('/test/file1', contents='abc')
+        self.fs.CreateFile('/test/file2', contents='abc')
+        sys.argv = ['finddup', '/test', '-v']
+
+        finddup.output.redirect("stdout.txt")
+        _old_stderr = sys.stderr
+        stderr_cap = io.StringIO()
+        sys.stderr = stderr_cap
+
+        main()
+
+        finddup.output.use_default()
+        sys.stderr = _old_stderr
+
+        with open('stdout.txt') as f:
+            self.assertRegex(stderr_cap.getvalue(), "^INFO:finddup:")
