ef459c62bb5af245b7cfe4b44a1b31273ca27934
Jeff Leverenz <jeff.leverenz@gmail.com>
2017-02-06 22:24:50 -0600

    Initial main tests.

---
+++ tests/test_main.py
@@ -0,0 +1,20 @@
+from test_helper import *
+from pyfakefs import fake_filesystem_unittest
+from finddup.__main__ import main
+import sys
+
+
+class TestMain(fake_filesystem_unittest.TestCase):
+
+    def setUp(self):
+        self.setUpPyfakefs()
+
+    def test_it(self):
+        self.fs.CreateFile('/test/file1', contents='abc')
+        self.fs.CreateFile('/test/file2', contents='abc')
+        sys.argv = ['finddup', '/test', '--output', 'results.txt']
+        main()
+
+        # Test that the results file was created and is empty.
+        with open('results.txt') as f:
+            self.assertEqual(f.read().strip(), "/test/file2")
